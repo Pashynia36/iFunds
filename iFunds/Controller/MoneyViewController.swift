d0805aa94e14e47851c001447f67f5abca492582
Pavlo Novak <>
2018-05-10 17:43:18 +0300

    TableView added

--- iFunds/Controller/MoneyViewController.swift
+++ iFunds/Controller/MoneyViewController.swift
@@ -26,3 +26,16 @@ class MoneyViewController: UIViewController {
         }
     }
 }
+
+extension MoneyViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+}
---
+++ iFunds/TransactionsTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TransactionsTableViewCell.swift
+//  iFunds
+//
+//  Created by Pavlo Novak on 5/10/18.
+//  Copyright © 2018 Pavlo Novak. All rights reserved.
+//
+
+import UIKit
+
+class TransactionsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageInTheCell: UIImageView!
+    @IBOutlet weak var purposeInTheCell: UILabel!
+    @IBOutlet weak var amountInTheCell: UILabel!
+}

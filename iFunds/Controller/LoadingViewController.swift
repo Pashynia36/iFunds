20fecf348ee1093291c5b18e2edbc7a36fe4d4dc
Pavlo Novak <>
2018-05-10 16:35:19 +0300

    Loading screen animations. Tab bar controllers.

---
+++ iFunds/Controller/LoadingViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoadingViewController.swift
+//  iFunds
+//
+//  Created by Pavlo Novak on 5/10/18.
+//  Copyright © 2018 Pavlo Novak. All rights reserved.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    @IBOutlet weak var iFundsLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var saveLabel: UILabel!
+    @IBOutlet weak var predictLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        self.iFundsLabel.center.y += self.view.bounds.height
+        self.countLabel.alpha = 0.0
+        self.saveLabel.alpha = 0.0
+        self.predictLabel.alpha = 0.0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 2.5, delay: 0.0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.1, options: [.curveEaseOut], animations: {
+            self.iFundsLabel.center.y -= self.view.bounds.height
+        }) { (finished) in
+            if finished {
+                UIView.animate(withDuration: 1.5, animations: {
+                    self.countLabel.alpha = 1.0
+                }, completion: { (finished) in
+                    if finished {
+                        UIView.animate(withDuration: 1.5, animations: {
+                            self.saveLabel.alpha = 1.0
+                        }, completion: { (finished) in
+                            if finished {
+                                UIView.animate(withDuration: 1.5, animations: {
+                                    self.predictLabel.alpha = 1.0
+                                }, completion: { (finished) in
+                                    if finished {
+                                        self.performSegue(withIdentifier: "homeLoad", sender: self)
+                                    }
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        }
+    }
+}

935340ecb9591d7cef2916ce3a143c712cc779ae
Pavlo Novak <>
2018-05-14 17:50:24 +0300

    Bar chart(diagram) added. TODO: fix graphView(graph goes under the view); test BarView.

--- iFunds/Controller/DiagramViewController.swift
+++ iFunds/Controller/DiagramViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class DiagramViewController: UIViewController {
+class DiagramViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
     @IBOutlet weak var containerConstant: NSLayoutConstraint!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var transactions: [Transaction] = []
+    var incomeValues: [Float] = []
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        getTransactions()
+        collectionView.register(BarCell.self, forCellWithReuseIdentifier: "barCell")
+        (collectionView?.collectionViewLayout as? UICollectionViewFlowLayout)?.scrollDirection = .horizontal
+        collectionView.backgroundColor = .gray
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -23,6 +37,44 @@ class DiagramViewController: UIViewController {
         if containerConstant.constant == 0 {
             containerConstant.constant -= 200
         }
+    }
+    
+    func getTransactions() {
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        do {
+            transactions = try managedContext.fetch(Transaction.fetchRequest())
+        } catch {
+            print("Fetching Failed")
+        }
+        for i in 0..<transactions.count {
+            if transactions[i].isIncome {
+                incomeValues.append(transactions[i].amount)
+            }
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return incomeValues.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "barCell", for: indexPath) as? BarCell, let maximum = incomeValues.max() {
+            
+            let value = incomeValues[indexPath.row]
+            let ratio = CGFloat(value / maximum)
+            cell.barHeightConstraint?.constant = CGFloat(collectionView.frame.height * ratio - 50.0)
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        return CGSize(width: 20, height: collectionView.frame.height)
     }
     
     @IBAction func showMenu(_ sender: UIBarButtonItem) {
---
+++ iFunds/View/BarCell.swift
@@ -0,0 +1,42 @@
+//
+//  BarCell.swift
+//  iFunds
+//
+//  Created by Pavlo Novak on 5/14/18.
+//  Copyright © 2018 Pavlo Novak. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+class BarCell: UICollectionViewCell {
+    
+    let barView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 3.0
+        return view
+    }()
+    
+    var barHeightConstraint: NSLayoutConstraint?
+    
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        
+        addSubview(barView)
+        
+        barHeightConstraint = barView.heightAnchor.constraint(equalToConstant: 300)
+        barHeightConstraint?.isActive = true
+        
+        barView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        barView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        barView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("coder wasn't implemented.")
+    }
+}

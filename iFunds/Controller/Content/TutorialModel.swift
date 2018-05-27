0a76859c398d2e967ef2a46cf8910d76097abdcc
Sergij Sobol <1sergij2007@gmail.com>
2018-05-27 13:07:03 +0300

    asset and storyboard fix.

--- iFunds/Controller/Content/TutorialModel.swift
+++ iFunds/Controller/Content/TutorialModel.swift
@@ -11,14 +11,14 @@ import UIKit
 
 final class TutorialContent {
     public let titles: [String] = {
-        return ["Hello!My name is Wezze. I’m the app that will forever save others from thinking about You: 'He/She got dressed not in the weather ...'",
-                "Now I need to know your geoposition for optimal composition of your looks for the next 3 days.Click Agree -> Allow.",
-                "Now I offer you different types of clothes that can be in your wardrobe, just follow instructions and enjoy result : ) Good luck!"]
+        return ["Let me introduce you the app that will help you save your money. This is iFunds.",
+                "All the given information is in easy-read style with each cell clickable and deletable.",
+                "Smart calculations will show you graph of your spents and incomes with charts."]
     }()
     
     public let images: [UIImage?] = {
-        return [UIImage(named: "trysiki"),
-                UIImage(named: "geo"),
-                UIImage(named: "hand")]
+        return [UIImage(named: "first"),
+                UIImage(named: "second"),
+                UIImage(named: "third")]
     }()
 }
